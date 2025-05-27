e3913f0a1b305e4fd958e06e21c03d4e5dd630a1
jh1662 <jh1662@canterbury.ac.uk>
2025-05-27 14:03:25 +0100

    got Flask server running.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # hackabury2025
 24 hour group project for Barclays Life Skills challenge
+
+# To do list:
+- For Barcleys
+- Teach people how to not be scammed
+- Scanning URLs, emails content, phones, etc
+- Guides
+- Secure links to Barcleys' pages
--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def entry():
+    return redirect(url_for('home'))
+
+@app.route("/home")
+def home():
     return "<p>Hello, World!</p>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
